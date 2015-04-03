4ae7055c5cc0589379196a51154ace839c7e1f2a
silvioheinze <silvioheinze@mailbox.org>
2015-04-04 00:30:13 +0200

    added new template for single prozess pages

---
+++ page-prozess.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+    <div class="row">
+	    <div class="large-8 columns">
+
+            <?php while ( have_posts() ) : the_post(); ?>
+  
+              <h2><?php the_title(); ?></h2>
+              <?php the_content(); ?>
+
+            <?php endwhile; ?>
+
+
+   		</div>
+
+    </div>
+
+<?php get_footer(); ?>
