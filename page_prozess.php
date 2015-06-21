544b9dcb8367b63337ebcf912f7ab04184cf7704
silvioheinze <silvioheinze@mailbox.org>
2015-06-21 07:10:27 +0200

    add live_stream_id to backend for ticker

--- page_prozess.php
+++ page_prozess.php
@@ -8,32 +8,33 @@ get_header(); ?>
     <div class="row">
 	    <div class="large-8 columns">
 
-            <?php while ( have_posts() ) : the_post(); ?>
+        <?php while ( have_posts() ) : the_post(); ?>
 
               <h2><?php the_title(); ?></h2>
               <?php the_content(); ?>
 
+              <?php $terminid = get_post_meta($post->ID, live_stream_id, true);
 
+              if ( $terminid[0] ):
+                  ?><h3>Termine</h3><?php
+                  echo do_shortcode('');
 
+              endif;
+              ?>
 
+           		</div>
 
+              <div class="medium-4 columns">
 
+                <?php $livestreamid = get_post_meta($post->ID, live_stream_id, true);
 
-   		</div>
+                if ( $livestreamid[0] ):
 
-      <div class="medium-4 columns">
+                  echo do_shortcode('[livestream streamid="'.$livestreamid.'" feedcount=5 credit=0]');
 
-        <?php
+                endif;
 
-        $livestreamid = get_post_meta($post->ID, live_stream_id, true);
-
-        if ( $livestreamid[0] ) {
-
-        echo do_shortcode('[livestream streamid="'.$livestreamid.'" feedcount=5 credit=0]');
-
-        }
-
-        endwhile; ?>
+            endwhile; ?>
 
 
       </div>
